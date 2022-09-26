eeb5cb413f1b26c03dbe281c9231beda111df87a
Siyam <itsyakubsiyam@gmail.com>
2022-09-27 02:09:36 +0600

    (feat): Mongo db connected

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// connect database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@digital-healthcare.2b0em69.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Healthcare db connected");
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Healthcare Server");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port ", port);
+});
