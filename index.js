1522679651ac153e2f188e61c0d23ef7ef92c113
Siyam <itsyakubsiyam@gmail.com>
2022-09-27 15:23:39 +0600

    add services

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(
-  cors({
-    origin: true,
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 //Server Connections
@@ -31,12 +25,20 @@ async function run() {
 
     const database = client.db("healthcaredb");
     const usersCollection = database.collection("users");
+    const servicesCollection = database.collection("services");
 
     // getting all users
     app.get("/users", async (req, res) => {
       const cursor = usersCollection.find({});
       const users = await cursor.toArray();
       res.send(users);
+    });
+
+    // getting all services
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
     });
   } finally {
     // await client.close();
